2c5fed5e1ef0e0dd17e7d7fddf4b71e009d15759
Diego97-webdev <shizusenpai97@gmail.com>
2020-09-22 19:22:05 -0300

    añadidas las animaciones con gsap sin intersection observer

--- js/app.js
+++ js/app.js
@@ -20,3 +20,77 @@ var swiper = new Swiper(".trabajos", {
   autoplay: true,
   speed: 1500,
 });
+
+gsap.from(".titulo-principal", {
+  duration: 0.5,
+  bottom: "-50px",
+  opacity: 0,
+});
+
+gsap.from(".descripcion-principal", {
+  delay: 0.5,
+  bottom: "-25px",
+  opacity: 0,
+});
+
+gsap.from(".btn-principal", {
+  delay: 1,
+  left: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".titulos-secciones", {
+  duration: 0.5,
+  delay: 5,
+  left: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".texto-comun", {
+  duration: 0.5,
+  delay: 5,
+  bottom: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".descripcion-segunda-seccion", {
+  duration: 0.5,
+  delay: 5,
+  bottom: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".contenedor-testimonios", {
+  duration: 0.5,
+  delay: 5,
+  bottom: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".trabajos", {
+  duration: 0.5,
+  delay: 5,
+  left: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".company-desc", {
+  duration: 0.5,
+  delay: 5,
+  bottom: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".ubicaciones", {
+  duration: 0.5,
+  delay: 5,
+  bottom: "-20px",
+  opacity: 0,
+});
+
+gsap.from(".nav-sec", {
+  duration: 0.5,
+  delay: 5,
+  bottom: "-20px",
+  opacity: 0,
+});
